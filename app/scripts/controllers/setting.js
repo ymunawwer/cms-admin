f6000811b46c944262617c79ec97de669472a493
vivek <vivek.aasaithambi@gmail.com>
2017-12-19 17:03:48 +0530

    settings controller changes

--- app/scripts/controllers/setting.js
+++ app/scripts/controllers/setting.js
@@ -12,28 +12,9 @@ angular.module('dmsAdminApp')
     settings.getSetting({}, {}, function (data) {
       $scope.settings = data.body.setting;
     });
-    $scope.$on('$viewContentLoaded', function () {
-      $('ul.tabs').tabs();
-    });
-    $scope.selectTab = function (id) {
-      $('ul.tabs').tabs('select_tab', id);
-    };
-    $scope.redirectPage = function (page) {
-      $state.go(page);
-    };
-    $scope.settingsTimpicker = {
-      dropdownToggleState: false,
-      time: {
-        fromHour: '05',
-        fromMinute: '30',
-        toHour: '10',
-        toMinute: '10'
-      },
-      theme: 'dark',
-      noRange: false,
-      format: 24,
-      noValidation: false
-    };
+    $scope.$on('$viewContentLoaded', function () { $('ul.tabs').tabs(); });
+    $scope.selectTab = function (id) { $('ul.tabs').tabs('select_tab', id); };
+    $scope.redirectPage = function (page) { $state.go(page); };
     var handleFileSelect = function (evt) {
       var valu = evt.currentTarget.files[0].type;
       var patt = new RegExp("image/");
@@ -68,7 +49,7 @@ angular.module('dmsAdminApp')
     $scope.updateSetting = function () {
 
       console.log($scope.settings);
-    //  fd.append("site_logo", $scope.blob, $scope.fileName);
+      //  fd.append("site_logo", $scope.blob, $scope.fileName);
       $scope.settings.site_logo = $scope.settings.blob;
 
       settings.updateSetting({}, $scope.settings, function (data) {
@@ -135,92 +116,92 @@ angular.module('dmsAdminApp')
     }
 
     $scope.myImage = '';
-      $scope.myCroppedImage = '';
-      $scope.cropType = "square";
-      $scope.validImage = false;
-      $scope.showCroppedImg = false;
+    $scope.myCroppedImage = '';
+    $scope.cropType = "square";
+    $scope.validImage = false;
+    $scope.showCroppedImg = false;
 
-      //Once the user uploads the image from file type input
-      var handleFileSelect = function (evt) {
-        var valu = evt.currentTarget.files[0].type;
-        var patt = new RegExp("image/");
-        if (patt.test(valu)) {
-          angular.element('#img_cropper').children('canvas').css('display', 'block');
-          $('#upload_block').css('display', 'none');
-          $('.cropArea').css('display', 'block');
-          $('.image_cropped').css('display', 'inline-block');
+    //Once the user uploads the image from file type input
+    var handleFileSelect = function (evt) {
+      var valu = evt.currentTarget.files[0].type;
+      var patt = new RegExp("image/");
+      if (patt.test(valu)) {
+        angular.element('#img_cropper').children('canvas').css('display', 'block');
+        $('#upload_block').css('display', 'none');
+        $('.cropArea').css('display', 'block');
+        $('.image_cropped').css('display', 'inline-block');
+        angular.element('#image_type').css('display', 'none');
+
+        var file = evt.currentTarget.files[0];
+        var reader = new FileReader();
+
+        var output = document.getElementById("result");
+
+        reader.onload = function (evt) {
+          $scope.$apply(function ($scope) {
+            $scope.myImage = evt.target.result;
+            $scope.validImage = true;
+          });
           angular.element('#image_type').css('display', 'none');
-
-          var file = evt.currentTarget.files[0];
-          var reader = new FileReader();
-
-          var output = document.getElementById("result");
-
-          reader.onload = function (evt) {
-            $scope.$apply(function ($scope) {
-              $scope.myImage = evt.target.result;
-              $scope.validImage = true;
-            });
-            angular.element('#image_type').css('display', 'none');
-          };
-          reader.readAsDataURL(file);
-          $("#fileInput").val = '';
-        } else {
-          $scope.validImage = false;
-          angular.element('#image_type').css('display', 'block');
-        }
-      };
-      angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
-
-      var mimeType = "image/jpeg"
-
-
-      function urltoFile(url, filename, mimeType) {
-        return (fetch(url).then(function (res) {
-          return res.arrayBuffer();
-        }).then(function (buf) {
-          return new File([buf], filename, { type: mimeType });
-        }));
-      }
-
-
-      // function to crop the uploaded images
-      $scope.crop = function () {
-        if ($scope.validImage) {
-          $scope.image_temp = $scope.myCroppedImage;
-
-          var byteString = atob($scope.myCroppedImage.split(',')[1]);
-
-          var ab = new ArrayBuffer(byteString.length);
-          var ia = new Uint8Array(ab);
-          for (var i = 0; i < byteString.length; i++) {
-            ia[i] = byteString.charCodeAt(i);
-          }
-          var blob = new Blob([ia], { type: 'image/png' });
-         console.log(blob);
-          blob.lastModifiedDate = new Date();
-          blob.lastModifiedDate = new Date();
-          $scope.settings.blob = blob;
-          $scope.settings.fileName = "a.png";
-
-          $scope.blobUrl = URL.createObjectURL(blob);
-          $scope.showCroppedImg = true;
-
-        } else {
-          Materialize.toast('<span>' +
-            "Upload valid image" +
-            '</span>',
-            3000);
-        }
-      }
-
-      //function to clear uploaded image
-      $scope.clearImage = function () {
-        $scope.showCroppedImg = false;
-        $scope.myImage = null;
-        $scope.validImage = false;
+        };
+        reader.readAsDataURL(file);
         $("#fileInput").val = '';
+      } else {
+        $scope.validImage = false;
+        angular.element('#image_type').css('display', 'block');
       }
+    };
+    angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
+
+    var mimeType = "image/jpeg"
+
+
+    function urltoFile(url, filename, mimeType) {
+      return (fetch(url).then(function (res) {
+        return res.arrayBuffer();
+      }).then(function (buf) {
+        return new File([buf], filename, { type: mimeType });
+      }));
+    }
+
+
+    // function to crop the uploaded images
+    $scope.crop = function () {
+      if ($scope.validImage) {
+        $scope.image_temp = $scope.myCroppedImage;
+
+        var byteString = atob($scope.myCroppedImage.split(',')[1]);
+
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+        }
+        var blob = new Blob([ia], { type: 'image/png' });
+        console.log(blob);
+        blob.lastModifiedDate = new Date();
+        blob.lastModifiedDate = new Date();
+        $scope.settings.blob = blob;
+        $scope.settings.fileName = "a.png";
+
+        $scope.blobUrl = URL.createObjectURL(blob);
+        $scope.showCroppedImg = true;
+
+      } else {
+        Materialize.toast('<span>' +
+          "Upload valid image" +
+          '</span>',
+          3000);
+      }
+    }
+
+    //function to clear uploaded image
+    $scope.clearImage = function () {
+      $scope.showCroppedImg = false;
+      $scope.myImage = null;
+      $scope.validImage = false;
+      $("#fileInput").val = '';
+    }
 
 
 
