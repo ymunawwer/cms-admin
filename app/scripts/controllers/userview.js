5f046131863e3775b73d14b1c14594306c68c519
sasikumar <sasi@gamasome.com>
2018-08-01 17:55:26 +0530

    code changes for refixing issue for MV-186-187

--- app/scripts/controllers/userview.js
+++ app/scripts/controllers/userview.js
@@ -46,7 +46,8 @@ angular.module('dmsAdminApp')
       'service_scheduler': 'SS',
       'service_adviser': 'SA',
       'vehicle​_inspection​': 'VI',
-      'admin': 'ADMIN'
+      'admin': 'ADMIN',
+      'user': 'USER'
     };
     $scope.printRoles = function (roles) {
 
